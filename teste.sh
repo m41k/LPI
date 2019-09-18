1caeee364f7d5cf6c503103715e3fb7bf8f76ac9
m41k <maik.alberto@hotmail.com>
2019-09-18 10:32:56 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-wget -qO - https://raw.githubusercontent.com/m41k/LPI/master/base/a > /tmp/ask1
-C=`head -1 /tmp/ask1`
-cat /tmp/ask1 | sed '1d'
-echo
-read R
-if [ $R = $C ]; then
- echo "correto"
-fi
 
+LISTA="https://raw.githubusercontent.com/m41k/quest/master/ls"
+wget -qO - $LISTA > /tmp/ls.lpi
+LISTA=/tmp/ls.lpi
+
+for L in $(cat $LISTA); do
+   sleep 2
+   clear
+   wget -qO - https://raw.githubusercontent.com/m41k/LPI/master/base/$L > /tmp/ask$L
+   C=`head -1 /tmp/ask$L`
+   cat /tmp/ask$L | sed '1d'
+   echo
+   read R
+   if [ $R = $C ]; then
+        echo "correto"
+   else
+        echo "errado"
+   fi
+done
